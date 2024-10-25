891aeac7ca80c4602d69f2b795d5aa30833c683c
NishakMohomed <nishakmohomed@gmail.com>
2024-10-25 09:59:21 +0530

    Added flowbite ui library and webcamjs cdn

--- resources/views/image-view.blade.php
+++ resources/views/image-view.blade.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Background remover</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css','resources/js/app.js'])
+
+    <!-- webcamjs for allowing sites to access device webcam -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js" integrity="sha512-dQIiHSl2hr3NWKKLycPndtpbh5iaHLo6MwrXm7F0FM5e+kL2U16oE9uIwPHUl6fQBeCthiEuV/rzP3MiAB8Vfw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
     
