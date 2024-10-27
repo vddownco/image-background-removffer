a3c394e57460600dcf1d130027510b80345551b4
NishakMohomed <nishakmohomed@gmail.com>
2024-10-27 14:52:47 +0530

    Made changes to ui

--- resources/views/livewire/background-remover.blade.php
+++ resources/views/livewire/background-remover.blade.php
@@ -2,21 +2,18 @@
 <div class="container mx-auto px-4 py-6">
     <h2 class="text-center text-blue-600 text-xl md:text-2xl font-semibold mb-6">Background remover</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-y-4 md:gap-x-2 justify-items-center pt-5">
-        <div class="flex flex-col justify-between items-center h-96 w-full">
-            <img src="https://tailwindui.com/plus/img/ecommerce-images/home-page-02-edition-01.jpg"
-                alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
-                class="h-full w-full object-cover object-center">
-            <div class="flex justify-center space-x-2 pt-5">
-                <button class="bg-blue-500 text-white px-4 py-2 rounded">Button 1</button>
-                <button class="bg-blue-500 text-white px-4 py-2 rounded">Button 2</button>
+        <div class="flex flex-col justify-between items-center p-1 h-96 w-full rounded bg-gray-200 border-gray-950">
+
+            <div class="h-full w-full rounded bg-gray-300 border-gray-950"></div>
+            <div class="flex justify-center space-x-2 py-2">
+                <button class="bg-emerald-500 hover:bg-emerald-600 text-white px-4 py-2 rounded">Button 1</button>
+                <button class="bg-emerald-500 hover:bg-emerald-600 text-white px-4 py-2 rounded">Button 2</button>
             </div>
         </div>
 
         <!-- -----Result section------- -->
-        <div class="flex justify-center items-center h-96 w-full">
-            <img src="https://tailwindui.com/plus/img/ecommerce-images/home-page-02-edition-01.jpg"
-                alt="Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug."
-                class="h-full w-full object-cover object-center">
+        <div class="flex justify-center items-center h-96 w-full rounded bg-gray-200 border-gray-950">
+
         </div>
     </div>
 </div>
