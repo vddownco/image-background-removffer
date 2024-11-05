a054ca304fd2fddb34c805a10a831905728a85f1
NishakMohomed <nishakmohomed@gmail.com>
2024-11-05 12:48:24 +0530

    Added laravel breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import "./bootstrap";
 import "flowbite";
 import ImageCompare from "image-compare-viewer";
+import Alpine from "alpinejs";
 
 window.ImageCompare = ImageCompare;
+
+window.Alpine = Alpine;
+
+Alpine.start();
