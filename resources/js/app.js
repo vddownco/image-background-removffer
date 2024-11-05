cd0dbc470da014e77f63021a98ec0553337c4070
NishakMohomed <nishakmohomed@gmail.com>
2024-11-05 15:16:21 +0530

    Fixed alpine js errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 import "./bootstrap";
 import "flowbite";
 import ImageCompare from "image-compare-viewer";
-import Alpine from "alpinejs";
+//import Alpine from "alpinejs";
 
 window.ImageCompare = ImageCompare;
 
-window.Alpine = Alpine;
+//window.Alpine = Alpine;
 
-Alpine.start();
+//Alpine.start();
