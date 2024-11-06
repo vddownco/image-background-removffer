612cdb3be5e1ddb4e99974e8ea0be3e0387bf22b
NishakMohomed <nishakmohomed@gmail.com>
2024-11-06 10:57:15 +0530

    SOSH2:Created url submit page and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\BackgroundRemover;
@@ -14,6 +15,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Posts resource routes
+    Route::resource('/post', PostController::class);
+    Route::resource('/post/create', PostController::class);
 });
 
 require __DIR__ . '/auth.php';
