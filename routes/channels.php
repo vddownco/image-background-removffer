f45aa3c93b09828d11d95c36746e5d90ca66a085
NishakMohomed <nishakmohomed@gmail.com>
2024-11-12 14:05:27 +0530

    SOSH7:made the post-genrated event into private route

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('post-generated.{postId}', function (User $user, string $postId) {
+    $post = Post::find($postId);
+    return $post && $user->id === $post->user_id;
 });
